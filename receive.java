6ca8a95f7e9b0aea5e681bb7972a60edbc1fa208
Digvijaysinh S Pawar <digvijaysinhpawar@Digvijaysinhs-MacBook-Pro.local>
2024-10-21 21:25:32 +0530

    receive update

---
+++ receive.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+
+import javax.xml.crypto.Data;
+
+import com.google.flatbuffers.FlatBufferBuilder;
+import Game.ClientMessage;
+
+public class receive implements Runnable {
+    private DatagramPacket packet;
+
+    public receive(DatagramPacket packet) {
+        this.packet = packet;
+    }
+
+    public void receiveMessage() {
+        byte[] buffer = packet.getData();
+        ByteBuffer buff = ByteBuffer.wrap(buffer);
+        ClientMessage message = ClientMessage.getRootAsClientMessage(buff);
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+                Server.udpSocket.receive(packet);
+                this.packet = packet;
+                receiveMessage();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+}
